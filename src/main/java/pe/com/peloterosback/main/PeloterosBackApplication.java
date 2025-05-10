8441cd67ee5857a0db7f0ca1099b6a7d95f56496
Harvey7557 <u24212352@utp.edu.pe>
2025-05-09 23:50:52 -0500

    commit inicial del proyecto Peloteros Spring boot

---
+++ src/main/java/pe/com/peloterosback/main/PeloterosBackApplication.java
@@ -0,0 +1,13 @@
+package pe.com.peloterosback.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeloterosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeloterosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/peloterosback/main/PeloterosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.peloterosback.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeloterosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
